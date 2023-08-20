7655a59263868f9d8ba6bcc0f6b4cfaaee0a38b5
Modesto Cabral <mcp.modesto@gmail.com>
2023-08-20 16:46:49 +0200

    Actualizado centralizado subida de ficheros. Creación de carpetas.

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 
 class Server {
 
@@ -13,7 +14,8 @@ class Server {
             auth: '/api/auth',
             categorias: '/api/categorias',
             productos: '/api/productos',
-            buscar: '/api/buscar'
+            buscar: '/api/buscar',
+            uploads: '/api/uploads'
         };
 
         // Conectar a base de datos
@@ -37,6 +39,13 @@ class Server {
 
         // Directorio público.
         this.app.use(express.static('public'));
+
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath : true
+        }));
     }
 
     routes() {
@@ -45,6 +54,7 @@ class Server {
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.buscar, require('../routes/buscar'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
     }
 
     listen() {
